57855e4d566e0a3766206a6cc5db5941ec3cf521
Gashaye Adugna <gasha.como@gmail.com>
2023-02-03 11:42:30 -0800

    inhertance

---
+++ Inhertance.py
@@ -0,0 +1,12 @@
+class Animal:
+    def eat(self):
+        print("animal has charater of eating")
+    def sleep(self):
+        print("eat")
+class dog(Animal):
+    def bark(self):
+        print("bark")
+ob=dog()
+ob.eat()
+ob.sleep()
+ob.bark()
